c8f125aaa68e75feb363e8e1517d6a66c5e15358
Pradip2003-web <pradipsavaliya2003@gmail.com>
2026-07-06 18:36:28 +0530

    Enhance admin registration and login with bcrypt and JWT
    Hash password during registration and implement JWT token generation on successful login.

--- backend/controller/admincontroller.js
+++ backend/controller/admincontroller.js
@@ -1,7 +1,10 @@
 var admin = require("../model/adminmodel");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 exports.register = async (req, res) => {
   try {
+     req.body.password = await bcrypt.hash(req.body.password, 10);
     var data = await admin.create(req.body);
     res.status(201).json({
       success: true,
@@ -20,15 +23,31 @@ exports.login = async (req, res) => {
   try {
     var data = await admin.find({ email: req.body.email });
     if (data.length != 0) {
-      res.status(200).json({
-        success: true,
-        message: "Admin Login Successfully",
-        data,
-      });
+          bcrypt.compare(
+        req.body.password,
+        data[0].password,
+        function (err, result) {
+          if (result == true) {
+            var token = jwt.sign({ _id: data[0]._id }, "admin", {
+              expiresIn: "1800s",
+            });
+            res.status(200).json({
+              message: "login successfully",
+              data,
+              token,
+            });
+          } else {
+            res.status(401).json({
+              status: false,
+              message: "Invalid Email And Password",
+            });
+          }
+        },
+      );
     } else {
-      res.status(401).json({
-        success: true,
-        message: "Invalid Email and Password",
+      res.status(400).json({
+        status: false,
+        message: "Invalid Email And Password",
       });
     }
   } catch (error) {
