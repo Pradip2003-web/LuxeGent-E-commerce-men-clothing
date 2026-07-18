a87ac98c02192b7768393cf96cc06a8e64152826
Pradip2003-web <pradipsavaliya2003@gmail.com>
2026-07-18 09:19:40 +0530

    Fix localStorage token

--- frontend/src/Pages/AdminLogin.jsx
+++ frontend/src/Pages/AdminLogin.jsx
@@ -73,8 +73,7 @@ function AdminLogin({ setAuthenticated }) {
         password,
       });
       if (res.data) {
-        localStorage.setItem('userToken',res
-                            data.token)
+        localStorage.setItem('userToken',res.data.token)
         toast.success("Admin login successfully");
         if (setAuthenticated) setAuthenticated(true);
         navigate("/admin/dashboard");
